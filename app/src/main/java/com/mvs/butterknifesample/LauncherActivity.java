9f9857023241186dd503d49acfa097201036d4a1
devmanjun <devmanjun@gmail.com>
2017-03-28 00:09:22 +0530

    Initial commit

---
+++ app/src/main/java/com/mvs/butterknifesample/LauncherActivity.java
@@ -0,0 +1,35 @@
+package com.mvs.butterknifesample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+
+public class LauncherActivity extends AppCompatActivity {
+    @BindView(R.id.tv_test) TextView sampleTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+        ButterKnife.bind(this);
+        init();
+
+    }
+
+    private void init() {
+        sampleTextView.setText("Test");
+    }
+
+    @OnClick(R.id.bt_button)
+    public void submitProcess()
+    {
+        Toast.makeText(LauncherActivity.this, "Button clicked", Toast.LENGTH_SHORT).show();
+        sampleTextView.setText("Text Changed");
+    }
+}
